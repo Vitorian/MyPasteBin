3fdf8edfc116802e14318280d5fea04528c56c38
Henrique Bucher <hbucher@gmail.com>
2019-02-17 14:59:28 -0600

    Added commented compiler boilerplate

--- CustomSTLAllocatorTest.cpp
+++ CustomSTLAllocatorTest.cpp
@@ -8,6 +8,20 @@ class myallocator
 public:
     using value_type    = T;
 
+    /*
+     // Boilerplate the compiler will fill out for you
+     using pointer       = value_type*;
+     using const_pointer = typename std::pointer_traits<pointer>::template
+                                                     rebind<value_type const>;
+     using void_pointer       = typename std::pointer_traits<pointer>::template
+                                                           rebind<void>;
+     using const_void_pointer = typename std::pointer_traits<pointer>::template
+                                                           rebind<const void>;
+     using difference_type = typename std::pointer_traits<pointer>::difference_type;
+     using size_type       = std::make_unsigned_t<difference_type>;
+     template <class U> struct rebind {typedef myallocator<U> other;};
+     */
+
     myallocator() noexcept {}  // not required, unless used
     template <class U> myallocator(myallocator<U> const&) noexcept {}
 
@@ -25,6 +39,37 @@ public:
         std::cout << "myallocator("<< this << ")::deallocate called " << p << std::endl;
         ::operator delete(p);
     }
+
+    /*
+      // More boilerplate the compiler will fill out for you
+      value_type* allocate(std::size_t n, const_void_pointer) {
+          return allocate(n);
+      }
+
+      template <class U, class ...Args>
+      void construct(U* p, Args&& ...args) {
+          ::new(p) U(std::forward<Args>(args)...);
+      }
+
+      template <class U>
+      void destroy(U* p) noexcept {
+         p->~U();
+      }
+
+      std::size_t  max_size() const noexcept {
+         return std::numeric_limits<size_type>::max();
+      }
+
+      myallocator select_on_container_copy_construction() const {
+         return *this;
+      }
+
+      using propagate_on_container_copy_assignment = std::false_type;
+      using propagate_on_container_move_assignment = std::false_type;
+      using propagate_on_container_swap            = std::false_type;
+      using is_always_equal                        = std::is_empty<myallocator>;
+  */
+
 private:
 
 };
